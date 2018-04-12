6e89733f2584b9c06677f5bd700f74686402e21a
Mike Scott <michael@lucidagency.com>
2018-04-11 19:18:46 -0700

    Adding line to update db task done time

--- social_media_takeover/instagram/api/db_tasks/set/clean_old_tag_interests.php
+++ social_media_takeover/instagram/api/db_tasks/set/clean_old_tag_interests.php
@@ -3,6 +3,12 @@
 
 require getcwd().'/../../lib/h.php';
 
+// update task done date
+$sql = "UPDATE `ig_db_tasks` SET `task_done`='";
+$sql .= date('Y-m-d');
+$sql .= "' WHERE `task_name` = 'clean_old_tag_interests';";
+sql_set_query( $sql );
+
 $clean_date =  date('Y-m-d',strtotime("-3 months"));
 	
 $sql = "DELETE FROM `ig_user_tag_interest` WHERE `freshness` < '".$clean_date."';";
--- social_media_takeover/instagram/api/db_tasks/set/clean_old_users.php
+++ social_media_takeover/instagram/api/db_tasks/set/clean_old_users.php
@@ -3,6 +3,12 @@
 
 require getcwd().'/../../lib/h.php';
 
+// update task done date
+$sql = "UPDATE `ig_db_tasks` SET `task_done`='";
+$sql .= date('Y-m-d');
+$sql .= "' WHERE `task_name` = 'clean_old_users';";
+sql_set_query( $sql );
+
 $clean_date =  date('Y-m-d',strtotime("-4 months"));
 
 $sql = "DELETE FROM `ig_users` WHERE `freshness` < '".$clean_date."';";
